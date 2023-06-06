1174dbee4034f473e8c9d8ee23c87fea22915749
unknown <jafri.taizun.s@gmail.com>
2023-06-06 15:51:37 +0530

    Added CNN class from another working project

--- KAGGLE/Digit_Recognizer/CNN_MODEL.py
+++ KAGGLE/Digit_Recognizer/CNN_MODEL.py
@@ -26,6 +26,24 @@ class dig_rec(nn.Module):
 
         return x
 
+class CNN(nn.Module):
+
+    def __init__(self,input_size = 1, num_classes = 10):
+        super(CNN,self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=8,kernel_size=(3,3), stride=(1,1), padding=(1,1))
+        self.pool = nn.MaxPool2d(kernel_size=(2,2), stride=(2,2))
+        self.conv2 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        self.fc1 = nn.Linear(16*7*7, num_classes) #Fully conncected layer
+
+    def forward(self,x) :
+        x = F.relu(self.conv1(x))
+        x = self.pool(x)
+        x = F.relu(self.conv2(x))
+        x = self.pool(x)
+        x = x.reshape(x.shape[0], -1)
+        x = self.fc1(x)
+
+        return x
 
 class custom_call(Dataset):
     def __init__(self,img_df, label_df,dataset_csv):
